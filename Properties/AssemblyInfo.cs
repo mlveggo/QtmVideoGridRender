7000b1d96f69026ae462c36b2bf1d4f1d1eafaa9
Morgan Larsson <morgan.larsson@qualisys.se>
2020-11-16 09:10:40 +0100

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("QtmVideoGridRender")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Qualisys / Morgan Larsson")]
+[assembly: AssemblyCompany("github.com/mlveggo")]
 [assembly: AssemblyProduct("QtmVideoGridRender")]
-[assembly: AssemblyCopyright("Copyright © Qualisys 2020")]
+[assembly: AssemblyCopyright("Copyright © github.com/mlveggo 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
